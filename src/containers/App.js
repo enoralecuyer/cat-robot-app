99150fb5b3fe25c8e080c4bd691374565cdb6ec0
enoralecuyer <hey.enora@gmail.com>
2020-05-05 15:21:23 -0700

    first commit

---
+++ src/containers/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Emoji from '../components/Emoji';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      robots: [],
+      searchfield: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response=> response.json())
+      .then(users => {this.setState({ robots: users})});
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchfield: event.target.value })
+  }
+
+  render() {
+    const { robots, searchfield } = this.state;
+    const filteredRobots = robots.filter(robot =>{
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
+    })
+    return !robots.length ?
+      <h1>Loading</h1> :
+      (
+        <div className='tc'>
+          <h1 className='f1'><Emoji symbol="🐈" label="cat"/> Cat Robot App <Emoji symbol="🐈" label="cat"/></h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+            <CardList robots={filteredRobots} />
+        </div>
+      );
+  }
+}
+
+export default App;
